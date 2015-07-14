cda6397fbb09e37391be973774b2748d3fda1254
xuzhenlei <1249756978@qq.com>
2015-07-14 09:35:16 +0800

    ok

---
+++ 统计学习方法 笔记/chapter2_Perception/原始形式.py
@@ -0,0 +1,48 @@
+# coding:utf-8
+'''
+Created on 2015年7月14日
+
+@author: Administrator
+'''
+import os
+training_set = [[(3, 3), 1], [(4, 3), 1], [(1, 1), -1]]
+w = [0, 0]
+b = 0
+
+def update(item):
+    global w, b
+    w[0] = w[0] + 1 * item[1] * item[0][0]
+    w[1] = w[1] + 1 * item[1] * item[0][1]
+    b = b + 1 * item[1]
+    print w,b
+    
+def cal(item):
+    global w,b
+    res = 0
+    for i in range(len(item[0])):
+        res += item[0][i]*w[i]
+    res += b
+    res *= item[1]
+    return res
+def check():
+    flag = False
+    for item in training_set:
+        if cal(item)<=0:
+            flag = True
+            update(item)
+            
+    if not flag:
+        print 'w='+str(w)+'b='+str(b)
+        os._exit(0)
+    flag = False
+   
+if __name__ =='__main__':
+    for i in range(1000):
+        check()
+
+
+
+
+
+
+
